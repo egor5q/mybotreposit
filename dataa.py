d12bf78f64197c5346c1d96cfade30f9ffd9561f
egor5q <31898567+egor5q@users.noreply.github.com>
2017-12-02 12:56:58 +0300

    Create dataa.py

---
+++ dataa.py
@@ -0,0 +1,39 @@
+import random
+x=0
+hod=0
+aaa=0
+per=0
+begin=0
+tt=6
+ler=0
+ren=0
+a=0
+tr=6
+predatornumber=1
+readys=0
+textt='Видимые локации: '
+chats=[]
+players=[]
+eaglepoints=0
+rabbitpoints=0
+wolfpoints=0
+animaleat=0
+w=0
+q=0
+z=0
+e=0
+k=0
+peremennaya=0
+gotov=0
+readynumber=[]
+readyplayers=0
+timers=[]
+seelocation=[]
+
+class Game(object):
+    def __init__(self):
+        self.person = {}
+        self.aidi = []
+        self.spisok = ['Eagle','Wolf']
+        
+game = Game()
